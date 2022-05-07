91fac0c01ddf2a4709d18d2790f9f0bbf6cc69ba
Carpe Noctem <noctem@carpenoctem.student.utwente.nl>
2022-05-07 15:20:37 +0200

    B_1

---
+++ B/infinity_area/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+void solve() {
+	int R, A, B;
+	std::cin << R << A << B;
+		
+}
+
+int main() {
+
+	int T(0);
+	std::cin >> T;
+
+	for(size_t t = 0; t < T; t++) {
+		solve();
+	}
+	return 0;
+}
